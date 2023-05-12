f7ccbe917be77d99c837c366998403023a1972ed
Rafael Vega <rafael.vega.tec@outlook.com>
2023-05-11 18:10:04 -0600

    Commit de prueba desde rama nueva

---
+++ CircleAreaCalculatorSol/CircleAreaCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CircleAreaCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.1416;
+            string userData;
+            double radius;
+            double circleArea;
+
+            Console.WriteLine("Proporcion el radio de tu circulo por favor:");
+            userData = Console.ReadLine();
+
+            if (double.TryParse(userData, out radius))
+            {
+                circleArea = PI * Math.Pow(radius, 2);
+                Console.WriteLine($"el area de tu circulo es: {circleArea}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Los datos proporcionados son incorrectos.");
+                Console.ReadKey();
+                Console.WriteLine("Los datos proporcionados son incorrectos.");
+            }
+
+        }
+    }
+}
---
+++ MessagePrinterSol/MessagePrinter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MessagePrinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = String.Empty;
+            string Message = String.Empty;
+
+            Console.WriteLine("Proporciona un mensaje por favor: ");
+            message = Console.ReadLine();
+            Console.WriteLine("Tu cadena es: " + Message);
+            Console.ReadKey();
+        }
+    }
+}
