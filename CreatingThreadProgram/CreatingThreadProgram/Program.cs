90583bffb6f1ecbab87c4b721d2e2ede86112c52
Rafael Vega <rafael.vega.tec@outlook.com>
2023-05-15 10:43:05 -0600

    Hilos

---
+++ CreatingThreadProgram/CreatingThreadProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CreatingThreadProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hilos
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.IsBackground = true;
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Hilo principal: Trabajando...");
+                Thread.Sleep(0);
+            }
+            Console.ReadKey();
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Proceso de hilo: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
