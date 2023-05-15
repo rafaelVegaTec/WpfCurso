b6fc1b96d10f6e8ca068cb1fc088d09ef0a6e4fc
Rafael Vega <rafael.vega.tec@outlook.com>
2023-05-15 15:03:31 -0600

    Cancelacion de Tareas

---
+++ AccessingShareDataSol/AccessingShareData/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AccessingShareData
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int n = 0;
+        //    var up = Task.Run(() =>
+        //    {
+        //        for (int i = 0; i < 1000000; i++)
+        //        {
+        //            n++;
+        //        }
+        //    });
+
+        //    for (int i = 0; i < 1000000; i++)
+        //    {
+        //        n--;
+        //    }
+        //    up.Wait();
+        //    Console.WriteLine(n);
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ AccessingShareDataSol/AccessingShareData/UsingCancellationToker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessingShareData
+{
+    internal class UsingCancellationToker
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+            }, token);
+
+            Console.WriteLine("Presiona ENTER para cancelar la tarea.");
+            Console.ReadLine();
+            cts.Cancel();
+            Console.WriteLine("Presiona ENTER para terminar la aplicacion");
+            Console.ReadKey();
+        }
+    }
+}
