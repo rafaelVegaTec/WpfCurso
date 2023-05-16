f25c1eed18e21495b964377e49de6cc19c647f86
Rafael Vega <rafael.vega.tec@outlook.com>
2023-05-16 15:25:19 -0600

    Directorios- incompletos

---
+++ ListingDriverInfoSol/ListingDriverInfo/CreatingNewDirectory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListingDriverInfo
+{
+    internal class CreatingNewDirectory
+    {
+        static void Main(string[] args)
+        {
+            var directory = Directory.CreateDirectory(@"C:\temp\AprendeProgramarConCsharp\Directory"); //Nos permite crear directamente el directorio carpeta
+            var directoryInfo = new DirectoryInfo(@"C:\temp\AprendeProgramarConCsharp\Directory");//necesita del metodo Create para crear un directorio carpeta
+            directoryInfo.Create();            
+            Console.WriteLine("Termine");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ListingDriverInfoSol/ListingDriverInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListingDriverInfo
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+        //    foreach(DriveInfo driveInfo in drivesInfo)
+        //    {
+        //        Console.WriteLine($" Drive {driveInfo.Name}");
+        //        Console.WriteLine($" File Type: {driveInfo.DriveType}");
+        //        if(driveInfo.IsReady == true)
+        //        {
+        //            Console.WriteLine($" Volume label: {driveInfo.VolumeLabel}");
+        //            Console.WriteLine($" File system: { 0}", driveInfo.DriveFormat);
+        //            Console.WriteLine($" Availleble space to current user: { driveInfo.AvailableFreeSpace} bytes");
+        //            Console.WriteLine($" Total availlable space: {driveInfo.TotalFreeSpace} bytes");
+        //            Console.WriteLine($" Total size of drive: {driveInfo.TotalSize} bytes");
+        //        }
+        //        Console.ReadKey();
+        //    }
+        //}
+    }
+}
