21045dfe8b33b4ac7a1fc54b8e728b78de0fe6f0
Rafael Vega <rafael.vega.tec@outlook.com>
2023-05-16 15:02:21 -0600

    Manejo de exepciones

---
+++ UsingDebugClassSol/UsingDebugClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace UsingDebugClass
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Debug.WriteLine("Stating application");
+        //    Debug.Indent();
+        //    int i = 1 + 2;
+        //    Debug.Assert(i == 4);
+        //    Debug.WriteLineIf(i > 0, "i is greater than 0");
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ UsingDebugClassSol/UsingDebugClass/UsingTraceSourceClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDebugClass
+{
+    internal class UsingTraceSourceClass
+    {
+        static void Main(string[] args)
+        {
+            TraceSource traceSource = new TraceSource("myTraceSource", SourceLevels.All);
+            traceSource.TraceInformation("Tracing application...");
+            traceSource.TraceEvent(TraceEventType.Critical, 0, "Critical trace");
+            traceSource.TraceData(TraceEventType.Information, 1, new object[] {"a", "b", "c"});
+            traceSource.Flush();
+            traceSource.Close();
+        }
+    }
+}
